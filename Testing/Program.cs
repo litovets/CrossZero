8dd07b2d11601aa5633242a4553d6e727bd4851d
Alexey Latushko <a.latushko@patrioticgames.org>
2013-12-04 21:28:14 +0200

    Added Testing project.

---
+++ Testing/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] data = { 1, 260, 300, 0 };
+            byte[] buffer = new byte[data.Length * 4];
+            Buffer.BlockCopy(data, 0, buffer, 0, buffer.Length);
+            foreach (byte b in buffer)
+            {
+                Console.Write(b + ", ");
+            }
+            Console.ReadLine();
+
+            int[] backData = new int[buffer.Length/4];
+            Buffer.BlockCopy(buffer, 0, backData, 0, buffer.Length);
+            foreach (int i in backData)
+            {
+                Console.Write(i + ", ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
